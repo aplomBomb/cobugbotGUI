8bd4ff645933b5bc239d33a36e21c03390db35e2
aplombomb <seizethesteez@gmail.com>
2019-03-04 01:09:01 -0500

    Create and Update

--- app/controllers/insults_controller.rb
+++ app/controllers/insults_controller.rb
@@ -8,8 +8,31 @@ class InsultsController < ApplicationController
   end
 
   def new
+    @insult = Insult.new
+  end
+
+  def create
+    insult = Insult.create(insult_params)
+
+    redirect_to insults_path
   end
 
   def edit
+    @insult = Insult.find(params[:id])
+    
   end
+
+  def update
+    @insult = Insult.find(params[:id])
+    @insult.update(insult_params)
+
+    redirect_to insult_path(@insult)
+  end
+
+  private
+
+  def insult_params
+    params.require(:insult).permit(:phrase, :contributor)
+  end
+
 end
