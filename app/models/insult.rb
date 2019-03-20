6417b94103c71d61c13b49ac8970c3c9ca2c5cad
aplombomb <seizethesteez@gmail.com>
2019-03-19 21:03:24 -0400

    model create/update validation to prohibit slurs

--- app/models/insult.rb
+++ app/models/insult.rb
@@ -1,2 +1,19 @@
 class Insult < ApplicationRecord
+    validate :you_cannot_be_racist
+
+    def you_cannot_be_racist
+      slurArray = ['nigger', 'jew', 'wetback', 'fag', 'faggot', 'porch monkey', 'spic', 'faggit', 'fagget', 'f4ggot', 'f4gg0t', 'jigaboo', ]
+      slurArray.any? do |s|
+        if phrase.include?(s)
+           errors.add(:phrase, "Racial/sexist slur used")
+        end 
+    end
+        slurArray.any? do |s|
+          if contributor.include?(s)
+            errors.add(:contributor, "Racial/sexist slur used")
+          end
+        end
+    end
 end
+
+
